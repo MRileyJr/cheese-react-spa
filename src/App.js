11528bda7bf164d221b8b2317e9aebf84fb6babc
MRileyJr <mrileyjr@yahoo.com>
2019-05-08 11:02:57 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import {BrowserRouter as Router} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Routes from "./Routes";
+import CheeseNav from './components/CheeseNav';
+import Footer from './components/Footer';
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <CheeseNav></CheeseNav>
+      <Routes>
+
+      </Routes>
+      <Footer></Footer>
+    </Fragment>
+  </Router>
+);
 
 export default App;

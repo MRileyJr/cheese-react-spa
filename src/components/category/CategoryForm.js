d3ebb4085d5f23b1daf0e76adc80874a28c4574e
MRileyJr <mrileyjr@yahoo.com>
2019-05-08 15:08:25 -0400

    Categories View and Form Complete

---
+++ src/components/category/CategoryForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
+import request from "../../utilities/api-request";
+
+class CategoryForm extends Component {
+  state = {
+    name: "",
+    disabled: true
+  };
+
+  handleInputChange = event => {
+    // the value attribute of the input that was changed
+    const { value } = event.target;
+    // true or false based on whether the value is invalid
+    const disabled = value.legnth < 3 || value.legnth > 15;
+
+    this.setState({ name: value, disabled });
+  };
+
+  // sets the value to an empty string to reset the form
+  resetForm = () => this.setState({ name: "" });
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { name } = this.state;
+    const { addCategory } = this.props;
+
+    const res = await request.post("./categories", { name });
+    const category = res.data;
+
+    //send the new category back to the <CategoriesView> Parent
+    addCategory(category);
+
+    this.resetForm();
+  };
+
+  render() {
+    const { disabled, name } = this.state;
+
+    return (
+      <Container className="text-center">
+        <h2>Create a Category</h2>
+        <Form>
+          <Form.Group as={Col} sm={{ offset: 4, span: 4 }}>
+            <Form.Label>Category Name</Form.Label>
+            <Form.Control
+              name="name"
+              value={name}
+              minLength="3"
+              maxLength="15"
+              onChange={this.handleInputChange}
+            />
+          </Form.Group>
+
+          <Button
+            type="submit"
+            variant="primary"
+            disabled={disabled}
+            onClick={this.handleSubmit}
+          >
+            Create
+          </Button>
+        </Form>
+        <hr />
+      </Container>
+    );
+  }
+}
+
+CategoryForm.propTypes = {
+  addCategory: PropTypes.func.isRequired
+};
+
+export default CategoryForm;
---
+++ src/views/CategoriesView.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import request from "../utilities/api-request";
+import CategoriesList from "../components/category/CategoriesList";
+import CategoryForm from "../components/category/CategoryForm";
+
+class CategoriesView extends Component {
+  state = {
+    categories:[],
+  };
+
+  async componentDidMount() {
+    // meaning execute an HTTP GET request at api-domain/categories
+    const res = await request.get("/categories");
+    const categories = res.data; // the response data is the category collection list
+
+    this.setState({categories});
+  }
+
+  addToCategories = category =>
+    this.setState(state => {
+      const { categories } = state;
+
+      const newCategory = [category]
+      return {categories:[...categories, newCategory]};
+    });
+
+  render() {
+    const { categories } = this.state;
+
+    return (
+      <Container>
+        <Row>
+            <CategoryForm>
+              addCategory = {this.addToCategories}
+            </CategoryForm> 
+        </Row>
+        <br />
+        <Row>
+            <CategoriesList>
+              categories = {categories}
+            </CategoriesList>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default CategoriesView;

eb1d88216653d8c5b9ffddc3bb87cde0e15d0f50
MRileyJr <mrileyjr@yahoo.com>
2019-05-10 11:14:45 -0400

    added a route

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import CheesesView from "./views/CheesesView";
 import CategoriesView from "./views/CategoriesView";
 import HomeView from "./views/HomeView";
 import MenusView from "./views/MenusView";
+import MenuView from "./views/MenuView";
 
 const Routes = () => (
     <Switch>
@@ -12,7 +13,11 @@ const Routes = () => (
         <Route exact path="/cheeses" component={CheesesView}/>
         <Route exact path="/categories" component={CategoriesView}/>
         <Route exact path="/menus" component={MenusView}/>
+        <Route exact path="/menu/menuID" component={MenuView}/>
     </Switch>
 );
+
+// const menuNumber = props => {
+    // const { menuID } = props.match.params;
 
 export default Routes;

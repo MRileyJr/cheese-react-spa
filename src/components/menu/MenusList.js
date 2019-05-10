884d2aa6aedf8344bdddc74a446bc6e2b171d0d6
MRileyJr <mrileyjr@yahoo.com>
2019-05-10 07:10:05 -0400

    created menus list

---
+++ src/components/menu/MenusList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import { menuType } from "../../utilities/prop-types";
+
+const createMenuRow = menu => (
+    <tr key={menu.id}>
+      <td>
+        <Link to={`/menus/${menu.id}`}>{menu.name}</Link>
+      </td>
+    </tr>
+  );
+
+const MenusList = (props) => {
+  const { menus } = props;
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2 className='text-center'>Menus</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} lg={{ span: 6, offset: 3 }}>
+          <Table size='sm' bordered>
+            <thead>
+              <tr>
+                <th>Name</th>
+              </tr>
+            </thead>
+            <tbody>
+                {menus.map(createMenuRow)}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+MenusList.propTypes = {
+    menus: PropTypes.arrayOf(menuType).isRequired,
+};
+
+export default MenusList;
